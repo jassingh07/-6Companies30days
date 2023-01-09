335b7d781682aa2f1eb986ee4b2e5f0e0794d5e4
jassingh07 <91901835+jassingh07@users.noreply.github.com>
2023-01-10 00:09:33 +0530

    Add files via upload

---
+++ factorial trailing zeroes.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll           long long int
+#define fl(i,m,n,p)  for(int i=m;i<n;i+=p)
+#define bl(i,m,n,p)  for(int i=m,i>=n;i-=p)
+#define pb(a)        push_back(a)
+#define vi           vector<int>
+#define all(v)       v.begin(),v.end()
+#define vlli         vector<ll>
+#define si           set<int>
+#define MOD          (int)(1e9+7)
+#define plli         pair<ll,ll>
+#define testin       ll t;cin >> t; while (t--)
+#define fel(x,v)     for(auto x:v)
+#define print(x,v)   fel(x,v) cout<<x<<' '
+#define FIO          ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define vvi          vector<vi>
+#define vvlli        vector<vlli>
+#define vc           vector<char>
+#define vvc          vector<vc>
+#define vb           vector<bool>
+class Solution {
+public:
+    int trailingZeroes(int n) {
+        int fivepower = 0;
+
+        int cur = 5;
+        while(true){
+            int multiples = n / cur;
+            fivepower += multiples;
+            if(!multiples) break;
+            cur *= 5;
+        }
+        return fivepower;
+    }
+};
+int main()
+{
+   return 0;
+}
